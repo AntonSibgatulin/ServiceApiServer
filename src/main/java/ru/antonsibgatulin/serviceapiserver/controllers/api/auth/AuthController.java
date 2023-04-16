4acd6eccae0275306ff7f8dbdd3bf9f3245f89de
Anton <89879639755a@gmail.com>
2023-04-16 09:23:14 +0300

    origin3

--- src/main/java/ru/antonsibgatulin/serviceapiserver/controllers/api/auth/AuthController.java
+++ src/main/java/ru/antonsibgatulin/serviceapiserver/controllers/api/auth/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.antonsibgatulin.serviceapiserver.controllers.api.auth.exception.UserPasswordNotValid;
 import ru.antonsibgatulin.serviceapiserver.controllers.api.auth.request.AuthRequest;
+import ru.antonsibgatulin.serviceapiserver.include.TokenGenerator;
 import ru.antonsibgatulin.serviceapiserver.user.TokenUser;
 import ru.antonsibgatulin.serviceapiserver.user.User;
 import ru.antonsibgatulin.serviceapiserver.user.repository.TokenUserRepository;
@@ -34,7 +35,7 @@ public class AuthController {
             throw new UserPasswordNotValid();
         }
 
-        tokenUserRepository.save(new TokenUser(null,user.getUserId(),));
+        tokenUserRepository.save(new TokenUser(null,user.getUserId(), TokenGenerator.generateTokenBy(user)));
 
         return user;
     }
